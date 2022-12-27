932c455c50c649d283cf092ea61c0dc1b0c5f22d
kammounomarindus@gmail.com <kammounomarindus@gmail.com>
2022-12-27 10:50:27 +0100

    spec user 25122022 10:50

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselService } from '../services/settings';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private carousel:CarouselService
+  ) { }
 
   ngOnInit() {
+    this.carousel.getCarouselalldata()
   }
 
 }
